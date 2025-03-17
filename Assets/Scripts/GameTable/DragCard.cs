c4f26614a0836090c027a68214573dafc6e568a5
Andrii Sevastianov <mrpkmail@gmail.com>
2025-03-18 00:16:32 +0200

    Move GameTable classes to namespace

--- Assets/Scripts/GameTable/DragCard.cs
+++ Assets/Scripts/GameTable/DragCard.cs
@@ -1,52 +1,55 @@
-using UnityEngine.EventSystems;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+namespace GameTable
 {
-    private GridManager _gridManager;
-    private RectTransform _rectTransform;
-
-    private Vector2 _oldPosition = Vector2.zero;
-    private Vector2 _originalPosition, _dragOffset;
-
-    private bool _isDragging = false, _canDrag = false;
-
-    private void Awake()
+    public class DragCard : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
-        _rectTransform = GetComponent<RectTransform>();
-        _gridManager = FindObjectOfType<GridManager>();
-    }
+        private GridManager _gridManager;
+        private RectTransform _rectTransform;
 
-    public void CardCanDrag()
-    {
-        _canDrag = true;
-        _oldPosition = _rectTransform.anchoredPosition;
-    }
+        private Vector2 _oldPosition = Vector2.zero;
+        private Vector2 _originalPosition, _dragOffset;
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        if (_canDrag)
+        private bool _isDragging = false, _canDrag = false;
+
+        private void Awake()
         {
-            _originalPosition = _rectTransform.anchoredPosition;
-            _dragOffset = eventData.position - new Vector2(_originalPosition.x, _originalPosition.y);
-            _isDragging = true;
-            _canDrag = false;
+            _rectTransform = GetComponent<RectTransform>();
+            _gridManager = FindObjectOfType<GridManager>();
         }
-    }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (_isDragging)
+        public void CardCanDrag()
         {
-            Vector2 currentPosition = eventData.position - _dragOffset;
-            _rectTransform.anchoredPosition = currentPosition;
-            _rectTransform.SetAsLastSibling();
+            _canDrag = true;
+            _oldPosition = _rectTransform.anchoredPosition;
         }
-    }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        _isDragging = false;
-        _rectTransform.anchoredPosition = _gridManager.PlaceOnGrid(gameObject.GetInstanceID(), _rectTransform.anchoredPosition, _rectTransform.sizeDelta, _oldPosition);
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (_canDrag)
+            {
+                _originalPosition = _rectTransform.anchoredPosition;
+                _dragOffset = eventData.position - new Vector2(_originalPosition.x, _originalPosition.y);
+                _isDragging = true;
+                _canDrag = false;
+            }
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (_isDragging)
+            {
+                Vector2 currentPosition = eventData.position - _dragOffset;
+                _rectTransform.anchoredPosition = currentPosition;
+                _rectTransform.SetAsLastSibling();
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _isDragging = false;
+            _rectTransform.anchoredPosition = _gridManager.PlaceOnGrid(gameObject.GetInstanceID(), _rectTransform.anchoredPosition, _rectTransform.sizeDelta, _oldPosition);
+        }
     }
 }
--- Assets/Scripts/GameTable/GridManager.cs
+++ Assets/Scripts/GameTable/GridManager.cs
@@ -1,144 +1,147 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour
+namespace GameTable
 {
-    [SerializeField] private float _maxSearchRange = 10;
-    [SerializeField] private float _gridSize = 110f;
-
-    private int _padding = 1;
-
-    private Dictionary<Vector2Int, int> _grid = new Dictionary<Vector2Int, int>();
-    private Dictionary<int, List<Vector2Int>> _objectInGrid = new Dictionary<int, List<Vector2Int>>();
-
-    public Vector2 PlaceOnGrid(int objectID, Vector2 position, Vector2 objectSizeDelta, Vector2 oldPosition)
+    public class GridManager : MonoBehaviour
     {
-        Vector2Int gridPos = CordToGrid(position);
-        Vector2 objectSize = objectSizeDelta / _gridSize;
+        [SerializeField] private float _maxSearchRange = 10;
+        [SerializeField] private float _gridSize = 110f;
 
-        RemoveFromGrid(objectID);
+        private int _padding = 1;
 
-        if (IsOccupied(gridPos, objectSize, _padding))
+        private Dictionary<Vector2Int, int> _grid = new Dictionary<Vector2Int, int>();
+        private Dictionary<int, List<Vector2Int>> _objectInGrid = new Dictionary<int, List<Vector2Int>>();
+
+        public Vector2 PlaceOnGrid(int objectID, Vector2 position, Vector2 objectSizeDelta, Vector2 oldPosition)
         {
-            Vector2Int? newGridPos = FindNearestAvailablePosition(gridPos, objectSize);
+            Vector2Int gridPos = CordToGrid(position);
+            Vector2 objectSize = objectSizeDelta / _gridSize;
 
-            if (newGridPos.HasValue)
+            RemoveFromGrid(objectID);
+
+            if (IsOccupied(gridPos, objectSize, _padding))
             {
-                PlaceObjectOnGrid(objectID, newGridPos.Value, objectSize);
-                return GridToCord(newGridPos.Value);
+                Vector2Int? newGridPos = FindNearestAvailablePosition(gridPos, objectSize);
+
+                if (newGridPos.HasValue)
+                {
+                    PlaceObjectOnGrid(objectID, newGridPos.Value, objectSize);
+                    return GridToCord(newGridPos.Value);
+                }
+                else
+                {
+                    PlaceObjectOnGrid(objectID, CordToGrid(oldPosition), objectSize);
+                    return oldPosition;
+                }
             }
             else
             {
-                PlaceObjectOnGrid(objectID, CordToGrid(oldPosition), objectSize);
-                return oldPosition;
+                PlaceObjectOnGrid(objectID, gridPos, objectSize);
+                return GridToCord(gridPos);
             }
         }
-        else
+
+        private void RemoveFromGrid(int objectID)
         {
-            PlaceObjectOnGrid(objectID, gridPos, objectSize);
-            return GridToCord(gridPos);
-        }
-    }
-
-    private void RemoveFromGrid(int objectID)
-    {
-        if (!_objectInGrid.ContainsKey(objectID))
-        {
-            return;
-        }
-
-        foreach (var cell in _objectInGrid[objectID])
-        {
-            _grid.Remove(cell);
-        }
-
-        _objectInGrid.Remove(objectID);
-    }
-
-    private Vector2Int? FindNearestAvailablePosition(Vector2Int startGridPos, Vector2 objectSize)
-    {
-        Queue<Vector2Int> searchQueue = new Queue<Vector2Int>();
-        HashSet<Vector2Int> visitedCells = new HashSet<Vector2Int>();
-
-        searchQueue.Enqueue(startGridPos);
-        visitedCells.Add(startGridPos);
-
-        Vector2Int[] searchDirections = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
-        int searchDepth = 0;
-
-        while (searchQueue.Count > 0 && searchDepth < _maxSearchRange)
-        {
-            int currentLevelSize = searchQueue.Count;
-            searchDepth++;
-
-            for (int i = 0; i < currentLevelSize; i++)
+            if (!_objectInGrid.ContainsKey(objectID))
             {
-                Vector2Int currentPos = searchQueue.Dequeue();
+                return;
+            }
 
-                if (!IsOccupied(currentPos, objectSize, 0))
+            foreach (var cell in _objectInGrid[objectID])
+            {
+                _grid.Remove(cell);
+            }
+
+            _objectInGrid.Remove(objectID);
+        }
+
+        private Vector2Int? FindNearestAvailablePosition(Vector2Int startGridPos, Vector2 objectSize)
+        {
+            Queue<Vector2Int> searchQueue = new Queue<Vector2Int>();
+            HashSet<Vector2Int> visitedCells = new HashSet<Vector2Int>();
+
+            searchQueue.Enqueue(startGridPos);
+            visitedCells.Add(startGridPos);
+
+            Vector2Int[] searchDirections = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+            int searchDepth = 0;
+
+            while (searchQueue.Count > 0 && searchDepth < _maxSearchRange)
+            {
+                int currentLevelSize = searchQueue.Count;
+                searchDepth++;
+
+                for (int i = 0; i < currentLevelSize; i++)
                 {
-                    return currentPos;
-                }
+                    Vector2Int currentPos = searchQueue.Dequeue();
 
-                foreach (var direction in searchDirections)
-                {
-                    Vector2Int nextPos = currentPos + direction;
-
-                    if (!visitedCells.Contains(nextPos))
+                    if (!IsOccupied(currentPos, objectSize, 0))
                     {
-                        searchQueue.Enqueue(nextPos);
-                        visitedCells.Add(nextPos);
+                        return currentPos;
+                    }
+
+                    foreach (var direction in searchDirections)
+                    {
+                        Vector2Int nextPos = currentPos + direction;
+
+                        if (!visitedCells.Contains(nextPos))
+                        {
+                            searchQueue.Enqueue(nextPos);
+                            visitedCells.Add(nextPos);
+                        }
                     }
                 }
             }
+
+            return null;
         }
 
-        return null;
-    }
-
-    private bool IsOccupied(Vector2Int gridPos, Vector2 size, int padding)
-    {
-        for (int i = gridPos.x - padding; i <= gridPos.x + (int)size.x + padding; i++)
+        private bool IsOccupied(Vector2Int gridPos, Vector2 size, int padding)
         {
-            for (int j = gridPos.y - padding; j <= gridPos.y + (int)size.y + padding; j++)
+            for (int i = gridPos.x - padding; i <= gridPos.x + (int)size.x + padding; i++)
             {
-                if (_grid.ContainsKey(new Vector2Int(i, j)))
+                for (int j = gridPos.y - padding; j <= gridPos.y + (int)size.y + padding; j++)
                 {
-                    return true;
+                    if (_grid.ContainsKey(new Vector2Int(i, j)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void MarkAreaOccupied(int objectID, Vector2Int gridPos, Vector2 objectSize)
+        {
+            if (!_objectInGrid.ContainsKey(objectID))
+                _objectInGrid[objectID] = new List<Vector2Int>();
+
+            for (int i = gridPos.x; i < gridPos.x + objectSize.x; i++)
+            {
+                for (int j = gridPos.y; j < gridPos.y + objectSize.y; j++)
+                {
+                    Vector2Int cell = new Vector2Int(i, j);
+                    _grid[cell] = objectID;
+                    _objectInGrid[objectID].Add(cell);
                 }
             }
         }
-        return false;
-    }
 
-    private void MarkAreaOccupied(int objectID, Vector2Int gridPos, Vector2 objectSize)
-    {
-        if (!_objectInGrid.ContainsKey(objectID))
-            _objectInGrid[objectID] = new List<Vector2Int>();
-
-        for (int i = gridPos.x; i < gridPos.x + objectSize.x; i++)
+        private void PlaceObjectOnGrid(int objectID, Vector2Int gridPos, Vector2 objectSize)
         {
-            for (int j = gridPos.y; j < gridPos.y + objectSize.y; j++)
-            {
-                Vector2Int cell = new Vector2Int(i, j);
-                _grid[cell] = objectID;
-                _objectInGrid[objectID].Add(cell);
-            }
+            MarkAreaOccupied(objectID, gridPos, objectSize);
         }
-    }
 
-    private void PlaceObjectOnGrid(int objectID, Vector2Int gridPos, Vector2 objectSize)
-    {
-        MarkAreaOccupied(objectID, gridPos, objectSize);
-    }
+        private Vector2Int CordToGrid(Vector2 position)
+        {
+            return new Vector2Int(Mathf.RoundToInt(position.x / _gridSize), Mathf.RoundToInt(position.y / _gridSize));
+        }
 
-    private Vector2Int CordToGrid(Vector2 position)
-    {
-        return new Vector2Int(Mathf.RoundToInt(position.x / _gridSize), Mathf.RoundToInt(position.y / _gridSize));
-    }
-
-    private Vector2 GridToCord(Vector2Int gridPos)
-    {
-        return new Vector2(gridPos.x * _gridSize, gridPos.y * _gridSize);
+        private Vector2 GridToCord(Vector2Int gridPos)
+        {
+            return new Vector2(gridPos.x * _gridSize, gridPos.y * _gridSize);
+        }
     }
 }
